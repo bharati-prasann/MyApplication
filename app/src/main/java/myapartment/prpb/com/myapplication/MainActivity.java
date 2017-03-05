185ef1fb131e624e4d00522158f317aa61849287
bharati-prasann <bharati.prasann@gmail.com>
2017-03-05 16:23:22 +0530

    Update MainActivity.java

--- app/src/main/java/myapartment/prpb/com/myapplication/MainActivity.java
+++ app/src/main/java/myapartment/prpb/com/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package myapartment.prpb.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import anroid.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=new TextView();
+        tv.setText("Hello");
     }
 }
